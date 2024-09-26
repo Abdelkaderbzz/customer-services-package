87a47c66099e5cb3bc73c7c6ac8acac4c15f7543
abdelkaderbouzmita@takiacademyteam.com <abdelkaderbouzmita@takiacademyteam.com>
2024-09-26 18:21:13 +0100

    revert version

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,26 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import dts from 'vite-plugin-dts';
-import path from 'path';
-
-// Custom Vite plugin to prepend 'use client' directive
-function addUseClient() {
-  return {
-    name: 'add-use-client',
-    enforce: 'pre',
-    transform(code, id) {
-      if (id.endsWith('.tsx') || id.endsWith('.jsx')) {
-        return `'use client';\n${code}`;
-      }
-      return code;
-    },
-  };
-}
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'index.ts'),
-      name: 'ViteButton',
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "ViteButton",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts(), addUseClient()],
+  plugins: [react(), dts()],
 });
